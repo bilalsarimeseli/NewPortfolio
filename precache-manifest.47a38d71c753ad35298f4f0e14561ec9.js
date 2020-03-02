e6c8d2fd3a7dd2824ce0104c87fcc3a87f23d97a
Bilal Sarimeseli <45982392+bilalsarimeseli@users.noreply.github.com>
2020-03-02 13:50:20 -0800

    Updates

---
+++ precache-manifest.47a38d71c753ad35298f4f0e14561ec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98126f57b0da43fc2090cf06f076aeae",
+    "url": "/NewPortfolio/./index.html"
+  },
+  {
+    "revision": "6687864291c7b1a20df6",
+    "url": "/NewPortfolio/./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e63ba44585c6cedb437",
+    "url": "/NewPortfolio/./static/js/2.8d4be27e.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/NewPortfolio/./static/js/2.8d4be27e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6687864291c7b1a20df6",
+    "url": "/NewPortfolio/./static/js/main.d4ae2dc0.chunk.js"
+  },
+  {
+    "revision": "c81c6a4393bc2affd733",
+    "url": "/NewPortfolio/./static/js/runtime-main.b81300d0.js"
+  }
+]);
